fe8c6db9cd95e8a944eae3e02df6971ba93edea0
Ivan Rodriguez <ivan.rodriguez@jobsity.com>
2016-03-05 13:26:18 -0500

    First commit

---
+++ scala-ubuntu-setup.sh
@@ -0,0 +1,39 @@
+# Install vim
+sudo apt-get install vim -y
+
+
+# Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+sudo apt-get update
+
+sudo apt-get install google-chrome-stable -y
+
+# Postgres
+sudo add-apt-repository "deb https://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
+wget --quiet -O - https://postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install postgresql-9.4 -y
+
+# Java 1.8
+sudo apt-get -y install python-software-properties
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update -y
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo apt-get install oracle-java8-installer
+
+
+# SBT
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+sudo apt-get update
+sudo apt-get install sbt -y
+
+# Open terminal here
+sudo apt-get install nautilus-open-terminal -y
+# Log out and log in for the changes to take effect
+
+
